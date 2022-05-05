766a75dc3f91d6d1b66c51ca79b4ef08507d5646
Bryce Karlins <bjk0024@auburn.edu>
2022-05-04 21:38:36 -0500

    Working model propagation and more plots
    notable thoughts include include:
    - integrated continuous pp (lol) lookup into model propagation
    - fought with theta for a while before giving up and interpolating dy (is this actually in rad?)
    - off-by-one errors can be fudged with enough interpolating :)

--- PathGenTest.m
+++ PathGenTest.m
@@ -3,12 +3,15 @@
 % acceleration of a semi-tractor system with a parameterized dynamic model
 
 clear
-clf
+% clf
+load('rng.mat');
+rng(s);
+
 %% Define Params
 m=17000;
 m_eff=m*1.05;
 r_eff = .527;
-T = 1500;
+T_max = 1000;
 N = 2.64;
 
 % load coefs
@@ -18,56 +21,74 @@ C_a =18;%kW
 
 
 %% Traverse Path and solve for a,v,p?
-[x,y,~] = pathGen(5/3,100,26400,0.25);
-
-dx = diff(x);
-dy = diff(y);
-figure(1)
-stackedplot([y(1:end-1)' dy'])
-
-x_d = zeros(1,length(x));
-x_d(1) = 20;
-x_dd = zeros(1,length(x));
-for i = 1:length(x)-1
-    % Compute Inputs
-    th = atan(dy(i)/dx(i));
-
-    % drive force at tire
-    F_e = T*N/r_eff;
-
-    % rolling resistance 
-    F_rr = C_rr*m*9.81*cos(th);
-    
-    % Aero Drag
-    F_d = C_dl*x_d(i).^2;
-    
-    % Accessory Resistance
-    % rpm = v*r_eff/N;
-    % P_a = 2.91/100+1000
-    F_a = C_a*10^3/x_d(i);
-    
-    % Grade Resistance
-    F_g = m*9.81*sin(th);
-    
-    % x_dd = 1/m_eff*(T*N*r_eff - C_dl*x_d^2 - C_rr*m*9.81*cos(th))
-    
-    x_dd(i+1) = (F_e-F_d-F_rr-F_a-F_g)/m_eff;
-    x_d(i+1) = x_d(5/3,100,26400);
-
-
-end
-%% new path function testing
-
-sp = pathGen(5/3,100,26400);
-x = 0:1:26400;
+pathLen = 26400;
+sp = pathGen(5/3,100,pathLen);
+x = 0:1:pathLen;
 y = ppval(sp,x);
 dx = diff(x);
 dy = diff(y);
+% figure(1)
+% stackedplot([y(1:end-1)' dy']);
+
+dt = 0.1;
+numSamp = 30*60/dt;
+T = zeros(1,numSamp);
+x = zeros(1,numSamp);
+x_d = zeros(1,numSamp);
+x_d(1) = 20;
+x_dd = zeros(1,numSamp);
+
+th = interp1(1:length(dy),dy,1:numSamp);
+h(1) = 0;
+for t = 1:numSamp-1
+    v_des = 25;
+    T(t) = 0.3*(v_des-x_d(t))*T_max; % dummy P copntroller
+    T(t) = min(T_max, max(0,T(t))); % constrain [0, t_max]
+
+    % Compute Forces
+    F_e(t) = T(t)*N/r_eff;    % drive force at tire
+    F_rr(t) = C_rr*m*9.81*cos(th(t)); % rolling resistance
+    F_d(t) = C_dl*x_d(t).^2;   % Aero Drag    
+    F_a(t) = C_a*10^3/x_d(t);  % Accessory Resistance
+    % rpm = v*r_eff/N;
+    % P_a = 2.91/100+1000
+    F_g(t) = m*9.81*sin(th(t));   % Grade Resistance
+
+    x_dd(t+1) = (F_e(t)-F_d(t)-F_rr(t)-F_a(t)-F_g(t))/m_eff;
+    x_d(t+1) = x_d(t)+x_dd(t+1)*dt;
+    x(t+1) = x(t) + (x_d(t+1)+x_d(t))/2*dt;
+
+    % Compute Inputs
+    h(t+1) = ppval(sp,x(t+1));
+    
+%     th(t+1) = atan((h(t+1)-h(t))/dt);
+%     dy2 = interp1(1:length(dy),dy,1:numSamp)
+%     th(t+1) = dy2(t+1);
+end
 figure(1)
-stackedplot([y(1:end-1)' dy'])
+stackedplot([interp1(1:pathLen,y(1:end-1),1:t+1)' th' interp1(1:pathLen,dy,1:t+1)' ...
+    T' x_dd' x_d' x'],'DisplayLabels', ...
+    ["Path" "\theta" "dy" "T" "acc" "vel" "pos"])
+
+figure(2)
+stackedplot([y(1:end-1)' interp1(1:t,F_e,1:pathLen)' interp1(1:t,F_rr,1:pathLen)' ...
+            interp1(1:t,F_d,1:pathLen)' interp1(1:t,F_a,1:pathLen)'...
+            interp1(1:t,F_g,1:pathLen)'],'DisplayLabels', ...
+            ["Path" "F_e" "F_rr" "F_d" "F_a" "F_g"])
+
+
+%% new path function testing
+% 
+% sp = pathGen(5/3,100,26400);
+% x = 0:1:26400;
+% y = ppval(sp,x);
+% dx = diff(x);
+% dy = diff(y);
+% figure(1)
+% stackedplot([y(1:end-1)' dy'])
 
 %% Path Generation Function
-function sp = pathGen(h_std, spacing, dist)
+function pp = pathGen(h_std, spacing, dist)
 % pathGen(), 1D Gaussian Random-Walk Path Generator
 % Generates a 1D random-walk of length dist with constant spacing and 
 % std dev h_std. outputs a cubic interpolated spline as a piecewise
@@ -79,7 +100,7 @@ function sp = pathGen(h_std, spacing, dist)
 %
 % See also: INTERP1 PPVAL SPLINE
 
-numSam = dist/spacing;
+    numSam = dist/spacing;
     x = linspace(0,dist,numSam);
 %     y = abs(h_std*randn(1,numSam));
     y = zeros(1,numSam);
@@ -90,7 +111,7 @@ numSam = dist/spacing;
     end
 
 %     x_i = 0:interp:dist;
-    sp = spline(x,y);
+    pp = spline(x,y);
 
 %     plot(x,y,'o',x_i,sp)
 end
